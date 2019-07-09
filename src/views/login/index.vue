1826146fcb0c68ccca88d21ec4c13eed3b89db17
1612519774 <1612519774@qq.com>
2019-07-09 17:55:38 +0800

    登录页面的完善

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,12 +2,34 @@
   <div class="login-container">
     <el-card class="login_box">
         <img src="../../assets/images/logo_index.png" alt="">
+        <el-form >
+            <el-form-item>
+                <el-input placeholder="请输入手机号码"></el-input>
+            </el-form-item>
+             <el-form-item>
+                <el-input style="width:380px" placeholder="请输入密码"></el-input>
+                <el-button style="float:right">验证密码</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-checkbox v-model="checked"></el-checkbox>
+                我已阅读并同意 <el-link type="primary" :underline="false">用户协议</el-link> 和 <el-link type="primary" :underline="false">隐私条款</el-link>
+            </el-form-item>
+            <el-form-item>
+                <el-button style="width:100%" type="primary">登录</el-button>
+            </el-form-item>
+        </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      checked: true
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -28,8 +50,11 @@ export default {}
     img{
        display: block;
        width: 200px;
-       margin: 0 auto;
+       margin: 5px auto 20px ;
     }
   }
+}
+.el-checkbox{
+    margin-right: 5px;
 }
 </style>
