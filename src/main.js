a81a6c9fa73b8e8e3a702cf42edf58eaf53a43b7
1612519774 <1612519774@qq.com>
2019-07-09 16:06:32 +0800

    修改之前文件夹建立错误

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+// 导包
+import ElementUI from 'element-ui'
+// 引入样式
+import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
-
+// 在全局范围注册组件（注册element-ui组件库）
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
-
-  render: h => h(App),
-  router
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+// 导入 VueRouter 包
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+// 在全局范围实力化
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+
+export default router
