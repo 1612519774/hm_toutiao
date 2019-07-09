df75a2e373e95f1df761b489e0da26b597e181ec
1612519774 <1612519774@qq.com>
2019-07-09 15:22:26 +0800

    使用vue-router并且配置规则

--- src/views/router/index.js
+++ src/views/router/index.js
@@ -3,5 +3,13 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 // 引入样式
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入 VueRouter 包
+import VueRouter from 'vue-router'
 // 在全局范围注册组件（注册element-ui组件库）
 Vue.use(ElementUI)
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+
+export default router
